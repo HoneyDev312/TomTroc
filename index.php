4d235928bad8b36c5e2561f94a9c38ec7cadb1f8
HoneyDev312 <honeydev312@gmail.com>
2026-03-17 11:21:19 +0100

    database setup

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 
 require_once 'config/config.php';
 require_once 'views/View.php';
+require_once 'models/Database.php';
+
+$db = Database::getInstance();
+$pdo = $db->getPDO();
+$stmt = $pdo->query('SELECT * FROM users');
+$rows = $stmt->fetchAll();
+var_dump($rows);
 
 $view = new View("Accueil", "home");
 $view->render("home");
