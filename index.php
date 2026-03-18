f595e20b2e21f8a6da7782b50937b0d4a76cc765
HoneyDev312 <honeydev312@gmail.com>
2026-03-18 05:18:37 +0100

    header nav setup

--- index.php
+++ index.php
@@ -4,11 +4,5 @@ require_once 'config/config.php';
 require_once 'views/View.php';
 require_once 'models/Database.php';
 
-$db = Database::getInstance();
-$pdo = $db->getPDO();
-$stmt = $pdo->query('SELECT * FROM user');
-$rows = $stmt->fetchAll();
-var_dump($rows);
-
 $view = new View("Accueil", "home");
 $view->render("home");
--- views/templates/main.php
+++ views/templates/main.php
@@ -11,17 +11,64 @@
     <link rel="stylesheet" href="/css/reset.css">
     <link rel="stylesheet" href="/css/variables.css">
     <link rel="stylesheet" href="/css/common.css">
+    <link rel="stylesheet" href="/css/header.css">
     <?php if (!empty($templateCss)): ?>
         <link rel="stylesheet" href="/css/<?= strtolower(htmlspecialchars($templateCss)) ?>.css">
     <?php endif; ?>
 </head>
 
 <body>
+    <header>
+        <nav aria-label="Navigation principale">
+            <div class="container flex">
+                <a href="/" class="logo-link">
+                    <div class="logo-wrapper">
+                        <img src="/assets/svg/logo_white.svg" alt="Logo Tom Troc">
+                    </div>
+                    <span class="logo-text">Tom Troc</span>
+                </a>
 
+                <ul class="link-wrapper flex">
+                    <li>
+                        <a href="/">
+                            <span>Accueil</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">
+                            Nos livres à l'échange
+                        </a>
+                    </li>
+
+                </ul>
+
+                <ul class="account-wrapper flex">
+                    <li>
+                        <a href="/" class="link-message flex">
+                            <img src="/assets/svg/message.svg" alt="">
+                            <span>Messagerie</span>
+                            <span class="message-counter flex">1</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/" class="link-account flex">
+                            <img src="/assets/svg/account.svg" alt="">
+                            <span>Mon compte</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/" class="link-connection flex">
+                            <span>Connexion</span>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+    </header>
     <main>
         <?= $content  ?>
     </main>
-
 </body>
 
 </html>
