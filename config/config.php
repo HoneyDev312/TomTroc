123c575f9e19c12ad0c4b4eca2d9464d514a44b7
HoneyDev312 <honeydev312@gmail.com>
2026-04-04 05:33:45 +0200

    Reapply "add sql files, change gitignore and improve README"
    This reverts commit 3152da29e95757163e0f7b7d9d34b8115c27f7a5.

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('TEMPLATE_VIEW_PATH', __DIR__ . '/../App/Views/templates/');
+define('MAIN_VIEW_PATH', TEMPLATE_VIEW_PATH . 'main.php');
+define('BOOK_IMAGE_BASE_URL_IMAGES',  '/assets/images/');
+define('BOOK_IMAGE_BASE_URL_USERS',  '/assets/users/');
+define('BOOK_IMAGE_BASE_URL_SVG',  '/assets/svg/');
+define('BOOK_IMAGE_BASE_URL_BOOKS',  '/assets/books/');
